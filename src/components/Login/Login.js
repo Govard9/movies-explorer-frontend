27f831d5f8b6b1e71331cbc44d3e8594733c2b71
govard9 <web-supsu@yandex.ru>
2023-06-19 16:29:36 +0700

    feat: Закончить страницу авторизации и error404.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,24 @@ import header__logo from "../../images/logo/header__logo.svg";
 
 function Login(props) {
     return (
-        <>
-        </>
+        <section className="register">
+            <div className="register__head">
+                <img src={header__logo} alt=" Многогранный зеленый круг." className="header__logo header__logo_left"/>
+                <h3 className="register__title">Рады видеть!</h3>
+            </div>
+            <form className="register__form">
+                <label className="register__label">E-mail</label>
+                <input type="email" value="pochta@yandex.ru" className="register__input"/>
+                <label className="register__label">Пароль</label>
+                <input type="password" className="register__input register__input_pass-red"/>
+                <span className="register__span-validation"></span>
+                <button className="register__button register__button_margin">Войти</button>
+            </form>
+            <div className="register__login-block">
+                <span className="register__text">Ещё не зарегистрированы?</span>
+                <a href="/signup" className="register__login">Регистрация</a>
+            </div>
+        </section>
     );
 }
 
