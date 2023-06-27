e0a5de2672f41cff68a67443d74d60fccf088109
govard9 <web-supsu@yandex.ru>
2023-06-27 18:00:57 +0700

    feat: Сделать изменения и добавить инфу в файл README.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,6 +10,12 @@ function Profile(props) {
                 <li className="profile__name">
                     <span className="profile__text">Имя</span>
                     <span className="profile__text">Виталий</span>
+                    {/* Если пользователь нажал на кнопку "Редактировать" то появляется инпут для ввода нового имени */}
+                    {/*<input*/}
+                    {/*    type="text"*/}
+                    {/*    className="register__input register__input_profile"*/}
+                    {/*    minLength="5"*/}
+                    {/*    maxLength="35"/>*/}
                 </li>
                 <li className="profile__line">
                     <div className="promo__line promo__line_profile"></div>
@@ -17,14 +23,21 @@ function Profile(props) {
                 <li className="profile__text">
                     <span className="profile__text">E-mail</span>
                     <span className="profile__text">pochta@yandex.ru</span>
+                    {/* Если пользователь нажал на кнопку "Редактировать" то появляется инпут для ввода нового email */}
+                    {/*<input*/}
+                    {/*    type="email"*/}
+                    {/*    className="register__input register__input_profile"*/}
+                    {/*    minLength="5"*/}
+                    {/*    maxLength="254"/>*/}
                 </li>
             </ul>
             <div className="profile__block-edit-out">
                 <button className="profile__link-edit-profile">Редактировать</button>
                 <button className="profile__link-logout">Выйти из аккаунта</button>
                 {/* Если пользователь нажал на кнопку "Редактировать" то появляется кнопка "Сохранить" */}
+                {/* Если произошла ошибка, то делаем кнопку disabled и добавляем модификатор register__button_disabled */}
                 {/*<span className="profile__error-info">При обновлении профиля произошла ошибка.</span>*/}
-                {/*<button className="register__button register__button_margin-none">Сохранить</button>*/}
+                {/*<button className="register__button register__button_margin-none register__button_disabled" disabled>Сохранить</button>*/}
             </div>
         </section>
     );
